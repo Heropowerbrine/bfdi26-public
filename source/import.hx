6187156538d062fca268505d907ae335ac946e10
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-04-16 13:25:47 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -57,6 +57,26 @@ import sys.thread.Thread;
 import crowplexus.iris.Iris;
 #end
 
+//Mobile Controls
+ //import mobile.flixel.FlxHitbox;
+ //import mobile.flixel.FlxVirtualPad;
+ //import mobile.flixel.input.FlxMobileInputID;
+ import mobile.backend.SUtil;
+ import mobile.backend.MobileScaleMode;
+   
+ //Android
+ #if android
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
+ #end
+
 //Flixel
 import flixel.sound.FlxSound;
 import flixel.FlxG;
@@ -71,6 +91,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxDestroyUtil;
 
 using StringTools;
 using funkin.backend.FlxObjectTools;
